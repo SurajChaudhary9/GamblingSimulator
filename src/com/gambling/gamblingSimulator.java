f7ed029fa9d6911769b6658d23751f3e87d35dc4
SurajChaudhary9 <surajchaudhary0707@gmail.com>
2022-02-09 23:59:19 +0530

    Added : Welcome to Gambling Program

---
+++ src/com/gambling/gamblingSimulator.java
@@ -0,0 +1,12 @@
+package com.gambling;
+
+public class gamblingSimulator {
+	
+	public static int STAKE = 100;
+	public static int BET = 1;
+	public static void main(String[] args) {
+
+	System.out.println("Welcome to Gambling Simulation");
+	
+	}
+}
