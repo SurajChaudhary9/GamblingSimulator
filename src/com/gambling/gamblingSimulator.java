71ee09583349df0c85f5beeb5248de8bbbbd2a94
SurajChaudhary9 <surajchaudhary0707@gmail.com>
2022-02-13 15:36:52 +0530

    Added : Check For Resign

--- src/com/gambling/gamblingSimulator.java
+++ src/com/gambling/gamblingSimulator.java
@@ -3,26 +3,38 @@ package com.gambling;
 public class gamblingSimulator {
 	public static int STAKE = 100;
 	public static int BET = 1;
-	
-public static void checkForWinner() {
-	
-	int TRIAL = (int) Math.floor(Math.random() * 2);//Random Value
-	System.out.println("Value Is " +TRIAL);
-	
-	if (TRIAL == 0) {   
-		System.out.println("Gambler Wins the game");
-		STAKE++; 
-													//Print Results as per Random value
-		System.out.println("Gambler looses the game");
-		STAKE--;	
+	public static final int WIN = 1;
+	public static int stake = 0;
+
+	public static void checkForWinner() {
+
+		int TRIAL = (int) Math.floor(Math.random() * 2);// Random Value
+		System.out.println("Value Is " + TRIAL);
+
+		if (TRIAL == 0) {
+			System.out.println("Gambler Wins the game");
+			STAKE++;
+			// Print Results as per Random value
+			System.out.println("Gambler looses the game");
+			STAKE--;
+		}
+
+		System.out.println("Gambler having stake after bet are of $ " + STAKE);// Stake Value
 	}
-	
-	System.out.println("Gambler having stake after bet are of $ " + STAKE);//Stake Value
-}
+
+	public static void checkForResign() {
+		int totalStake = 0;
+		while (stake != 50 && stake != -50) {
+			checkForWinner();
+		}
+		totalStake = stake + STAKE;
+		System.out.println("Total stake for resign for a day is  " + totalStake);
+
+	}
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to Gambling Simulation");
 		checkForWinner();
+		checkForResign();
 	}
-
 }
